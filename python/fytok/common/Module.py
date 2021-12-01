c786558895bbbe1b1bac49c894ea91d1f70930e7
salmon <yuzhi@ipp.ac.cn>
2021-12-01 16:14:41 +0800

    using common.Module replace spdm.flow.Actor

--- python/fytok/common/Module.py
+++ python/fytok/common/Module.py
@@ -6,13 +6,12 @@ from spdm.common.logger import logger
 from spdm.common.SpObject import SpObject
 from spdm.common.tags import _undefined_
 from spdm.data.Entry import Entry
-from spdm.data.Node import sp_property
-from spdm.flow.Actor import Actor
+from spdm.data.Node import Dict, Node, sp_property
 
 from .Misc import Identifier
 
 
-class Module(Actor):
+class Module(SpObject, Dict[Node]):
 
     def __new__(cls, desc=None, *args, **kwargs):
 
@@ -20,7 +19,7 @@ class Module(Actor):
 
         cls_name = None
 
-        if cls is not Actor and prefix is None:
+        if cls is not Module and prefix is None:
             pass
         elif isinstance(desc, collections.abc.Mapping):
             cls_name = desc.get("code", {}).get("name", None)
@@ -35,8 +34,21 @@ class Module(Actor):
         else:
             return object.__new__(cls)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, d=None,  /, time=None,  **kwargs):
+        Dict.__init__(self, d,  **kwargs)
+        self._job_id = 0  # Session.current().job_id(self.__class__.__name__)
+        self._time = time if time is not None else 0.0
+
+    def __del__(self):
+        # logger.debug(f"Delete Module {guess_class_name(self.__class__)}")
+        pass
+
+    @property
+    def time(self):
+        return self._time
+
+    def job_id(self):
+        return self._job_id
 
     @sp_property
     def code(self):
@@ -50,5 +62,11 @@ class Module(Actor):
     def comment(self) -> str:
         return self.get("comment", "")
 
-    def refresh(self, *args, **kwargs) -> float:
-        return super().refresh(*args, **kwargs)
+    def refresh(self,  *args, time=_undefined_, ** kwargs) -> float:
+        """
+            Function: update the current state of the Module without advancing the time.
+            Return  : return the residual between the updated state and the previous state
+        """
+        if time is not _undefined_:
+            self._time = time
+        return 0.0
