8e7d110fda5a3e6f177db570bf10152f9aaed986
salmon <yuzhi@ipp.ac.cn>
2023-08-30 14:28:20 +0800

    [working]test on fy_port

--- examples/show_eq.py
+++ examples/show_eq.py
@@ -1,16 +1,19 @@
 import pathlib
-
+import os
 import numpy as np
 from spdm.data.File import File
 from spdm.view.View import display
 from spdm.utils.logger import logger
 from fytok.modules.Equilibrium import Equilibrium
 
+GACODE_PATH=os.environ['GACODE_PATH']
+SP_OUTPUT=os.environ['SP_OUTPUT']
+
 if __name__ == "__main__":
 
-    output_path = pathlib.Path('/home/salmon/workspace/output')
+    output_path = pathlib.Path(SP_OUTPUT)
 
-    eq = Equilibrium("GEQdsk:///home/salmon/workspace/gacode/neo/tools/input/profile_data/g141459.03890")
+    eq = Equilibrium(f"GEQdsk://{GACODE_PATH}/neo/tools/input/profile_data/g141459.03890")
 
     display(eq, title=f"EQUILIBRIUM", output=output_path/"EQUILIBRIUM.svg")
 
