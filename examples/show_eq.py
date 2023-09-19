b74eb4ccb626af096188e9007222464b14d5826a
Salmon <yuzhi@ipp.ac.cn>
2023-09-19 20:07:55 +0800

    [working]

--- examples/show_eq.py
+++ examples/show_eq.py
@@ -18,15 +18,15 @@ os.environ["SP_DATA_MAPPING_PATH"] = f"{WORKSPACE}/fytok_data/mapping"
 if __name__ == "__main__":
     output_path = pathlib.Path(f"{WORKSPACE}/output/")
 
+    ############################################################################
+    # Example: gfile I/O,  equilibrium.dump()
+
     eq0 = Equilibrium(f"file+GEQdsk://{WORKSPACE}/gacode/neo/tools/input/profile_data/g141459.03890#equilibrium")
 
     display(eq0, title=f"EQUILIBRIUM", output=output_path/"EQUILIBRIUM.svg")
 
-    eq_dump = eq0.dump()
-
     with File(f"{output_path}/EQUILIBRIUM.gfile", mode="w", format="GEQdsk") as oid:
-        oid.write({"equilibrium": eq_dump})
-        oid.close()
+        oid.write({"equilibrium": eq0.dump()})
 
     eq1 = Equilibrium(f"file+GEQdsk://{WORKSPACE}/output/EQUILIBRIUM.gfile#equilibrium")
 
@@ -34,30 +34,32 @@ if __name__ == "__main__":
 
     logger.debug(eq1.time_slice.current.profiles_1d.q.__value__)
 
-    # shot = 70745
+    ##############################################################################
 
-    # tok = Tokamak(f"EAST+MDSplus://{WORKSPACE}/fytok_data/mdsplus/~t/?enable=efit_east&shot={shot}")
+    shot = 70745
 
-    # tok.refresh(time=2.0)
+    tok = Tokamak(f"EAST+MDSplus://{WORKSPACE}/fytok_data/mdsplus/~t/?enable=efit_east&shot={shot}")
 
-    # psi = tok.equilibrium.time_slice.current.profiles_2d[0].psi.__value__
+    tok.refresh(time=2.0)
 
-    # psi_min = psi.min()
+    psi = tok.equilibrium.time_slice.current.profiles_2d[0].psi.__value__
 
-    # psi_max = psi.max()
+    psi_min = psi.min()
 
-    # levels = np.arange(psi_min, psi_max, (psi_max-psi_min)/40)
+    psi_max = psi.max()
 
-    # for i in range(150):
-    #     display(tok.equilibrium,
-    #             title=f"EAST shot={shot} time={tok.equilibrium.time_slice.current.time:.2f}s ",
-    #             output=output_path / f"tok_east_{int(tok.equilibrium.time_slice.current.time*100)}.png",
-    #             transparent=False,
-    #             psi={"$matplotlib": {"levels": levels}}
-    #             )
-    #     logger.debug(f"Equilibrium [{i:5d}] time={tok.equilibrium.time_slice.current.time:.2f}s")
-    #     tok.advance()
+    levels = np.arange(psi_min, psi_max, (psi_max-psi_min)/40)
 
-    # # convert -delay 10 -loop 1 tok_east*.png tok_east_70754.mp4
+    for i in range(150):
+        display(tok.equilibrium,
+                title=f"EAST shot={shot} time={tok.equilibrium.time_slice.current.time:.2f}s ",
+                output=output_path / f"tok_east_{int(tok.equilibrium.time_slice.current.time*100)}.png",
+                transparent=False,
+                psi={"$matplotlib": {"levels": levels}}
+                )
+        logger.debug(f"Equilibrium [{i:5d}] time={tok.equilibrium.time_slice.current.time:.2f}s")
+        tok.advance()
 
-    # logger.debug(tok.equilibrium.time_slice.time)
+    # convert -delay 10 -loop 1 tok_east*.png tok_east_70754.mp4
+
+    logger.debug(tok.equilibrium.time_slice.time)
