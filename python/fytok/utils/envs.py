ad8233fafce7fbb2269b3f9fc6ad44700578bd6b
Salmon <yuzhi@ipp.ac.cn>
2023-10-19 18:45:22 +0800

    [working]

--- python/fytok/utils/envs.py
+++ python/fytok/utils/envs.py
@@ -2,8 +2,7 @@ from spdm.utils import envs
 import os
 
 FY_DEBUG = os.environ.get("FY_DEBUG", True)
-os.environ["SP_DEBUG"] = str(FY_DEBUG)
-
-envs.SP_DEBUG = FY_DEBUG
+# os.environ["SP_DEBUG"] = str(FY_DEBUG)
+# envs.SP_DEBUG = FY_DEBUG
 
 __all__ = ["FY_DEBUG"]
